7e0b8b435f881f5d274fa1672834df893b564d79
Pradeep <p76667@gmail.com>
2023-06-05 16:42:12 +0530

    final pizza commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,14 +5,31 @@ public class Main {
     DeluxePizza dp = new DeluxePizza(true);
     System.out.println(dp.getPrice());
     dp.addTakeaway();
-    dp.addExtraCheese();
+//    dp.addExtraCheese();
+    dp.addExtraToppings();
     dp.addTakeaway();
     System.out.println(dp.getBill());
 
+    DeluxePizza dp1 = new DeluxePizza(false);
+    System.out.println(dp1.getPrice());
+    dp1.addTakeaway();
+//    dp1.addExtraCheese();
+    dp1.addExtraToppings();
+//    dp1.addExtraCheese();
+    dp1.addTakeaway();
+    System.out.println(dp1.getBill());
+
     Pizza p = new Pizza(true);
-    p.addExtraCheese();
+    System.out.println(p.getPrice());
+//    p.addExtraCheese();
     p.addExtraToppings();
     p.addTakeaway();
     System.out.println(p.getBill());
+
+    Pizza p1 = new Pizza(false);
+    System.out.println(p1.getPrice());
+//    p1.addExtraCheese();
+    p1.addTakeaway();
+    System.out.println(p1.getBill());
   }
 }
